7de52f41c25d516e76f8340618042a04c549a88c
Administrator <user@email.com>
2020-02-02 18:32:17 +0800

    现在是能播了，最后我还是用的无头浏览器，哎，直接发请求有时候是搞不定啊，无头浏览器明显是简化了步骤，要不蓝奏这鬼会给你返回一些错误信息，下面我应该是要试图加快一下步骤，让无头浏览器try-catch等待。另外，你做m3u8视频的时候，也应该把文件做大点，毕竟蓝奏不限速，下载挺快，但是我总是解析小文件这是要时间的啊

--- src/main/java/com/eg/lanzouserver/LanzouController.java
+++ src/main/java/com/eg/lanzouserver/LanzouController.java
@@ -3,7 +3,15 @@ package com.eg.lanzouserver;
 import com.eg.lanzouserver.bean.MyFile;
 import com.eg.lanzouserver.repository.MyFileRepository;
 import com.eg.lanzouserver.repository.VideoRepository;
+import com.eg.lanzouserver.util.Constants;
 import com.eg.lanzouserver.util.LanzouUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
@@ -14,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.ref.PhantomReference;
 
 /**
  * @time 2020-02-01 19:47
@@ -39,6 +48,9 @@ public class LanzouController {
         return "<h1>hello</h1>";
     }
 
+
+    private WebDriver driver;
+
     /**
      * 根据tsId获取文件直链，因为这里已经是在浏览器播放了，需要返回重定向
      *
@@ -47,12 +59,32 @@ public class LanzouController {
      */
     @RequestMapping("getFile")
     @CrossOrigin
-    public String getFile(@RequestParam String tsId, HttpServletResponse response) {
+    public String getFile(@RequestParam String tsId) {
         tsId = tsId.replace(".js", "");
         MyFile myFile = myFileRepository.findMyFileByTsIdEquals(tsId);
         String shareId = myFile.getShareId();
+
+        if (driver == null) {
+//            ChromeOptions chromeOptions = new ChromeOptions();
+//            chromeOptions.setHeadless(true);
+//            driver = new ChromeDriver(chromeOptions);
+            driver = new PhantomJSDriver();
+        }
+        driver.get(Constants.LANZOU_URL + "/" + shareId);
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement a = driver.switchTo().
+                frame(driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[4]/iframe")))
+                .findElement(By.xpath("//*[@id=\"go\"]/a"));
+        String href = a.getAttribute("href");
+        System.out.println(href);
         System.out.println("getFile tsId = " + myFile.getTsId());
-        return "redirect:" + new LanzouUtil().getDirectUrl(shareId);
+
+//        return "redirect:" + new LanzouUtil().getDirectUrl(shareId);
+        return "redirect:" + href;
     }
 
 }
